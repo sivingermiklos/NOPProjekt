accbde594aa557f8ff88359f1523271d7a49d74d
domipiggie <basti.domonkos.2020i@bankitatabanya.hu>
2022-11-09 12:58:08 +0100

    Szökőév egy feltételbe téve

--- src/nopprojekt/NOPProjekt.java
+++ src/nopprojekt/NOPProjekt.java
@@ -13,10 +13,7 @@ public class NOPProjekt {
     }
 
     public static boolean isLeapYear(int year){
-        if (year % 4 == 0 && year % 100 != 0){
-            return true;
-        }
-        else if (year % 400 == 0){
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0){
             return true;
         }
         
