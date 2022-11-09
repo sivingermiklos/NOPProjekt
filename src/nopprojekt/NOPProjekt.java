bb42dc0485c2a8cdec188b3111f739b640c468bf
Sivinger Miklós <sivinger.miklos.martin.2020i@bankitatabanya.hu>
2022-11-09 12:27:46 +0100

    Fő programrész

--- src/nopprojekt/NOPProjekt.java
+++ src/nopprojekt/NOPProjekt.java
@@ -1,10 +1,21 @@
 package nopprojekt;
 
+import java.util.Scanner;
+
 public class NOPProjekt {
 
     
     public static void main(String[] args) {
+
+        Scanner bill = new Scanner(System.in);
         
+        do {
+        System.out.print("Szökőév évszám: ");
+        int evSzam = bill.nextInt();
+        }
+        while(!isLeapYear(evSzam));
+        
+        System.out.println(backwards(evSzam));
     }
     
 }
