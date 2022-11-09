81f4bf9e34355ecb758551d84060190c2430ab67
domipiggie <basti.domonkos.2020i@bankitatabanya.hu>
2022-11-09 12:16:33 +0100

    Szökőév metódus

--- src/nopprojekt/NOPProjekt.java
+++ src/nopprojekt/NOPProjekt.java
@@ -2,6 +2,17 @@ package nopprojekt;
 
 public class NOPProjekt {
 
+    public static boolean isLeapYear(int year){
+        if (year % 4 == 0 && year % 100 != 0){
+            return true;
+        }
+        else if (year % 400 == 0){
+            return true;
+        }
+        
+        return false;
+    }
+    
     
     public static void main(String[] args) {
         
