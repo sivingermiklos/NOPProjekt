9400eb34b26ed68fd1bd14524206f2b5bca3137c
ArcticFalcon12 <giczi.attila.istvan.2020i@bankitatabanya.hu>
2022-11-09 12:31:41 +0100

    P feladat
    Metódus ami egy számot kap és fordított String-et ad vissza

--- src/nopprojekt/NOPProjekt.java
+++ src/nopprojekt/NOPProjekt.java
@@ -1,10 +1,14 @@
 package nopprojekt;
 
 public class NOPProjekt {
-
-    
+    public static void backwards(int szam) {
+         String szamString = Integer.toString(szam);
+         StringBuilder backwardLine = new StringBuilder(szamString);
+         backwardLine.reverse();
+         String backwardString = backwardLine.toString();
+         System.out.println(backwardString);
+    }
     public static void main(String[] args) {
-        
     }
     
 }
